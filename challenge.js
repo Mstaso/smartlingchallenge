79343595ce809e80da6e056dfb6172a989a38319
malcolm <mstaso1@gmail.com>
2021-01-29 12:28:15 -0500

    authentication

--- challenge.js
+++ challenge.js
@@ -1,1 +1,24 @@
 const fetch = require("node-fetch");
+const credentials = require('./resources/credentials.json');
+
+function authentication() {
+    fetch('https://api.smartling.com/auth-api/v2/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+             'accepts': 'application/json',
+          },
+          body: JSON.stringify({
+              "userIdentifier": credentials.userIdentifier,
+              "userSecret": credentials.userSecret
+          })
+    }) 
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        let token = response.data
+      })
+}
+
+authentication()
+
